cc15456dc0181f5c4be91a7891be12275c9c673c
johnotieno <ocsjohnte@gmail.com>
2026-09-05 06:41:35 -0700

    Add files via upload

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine($"Hello {firstName}!");
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Nice to meet you, {firstName} {lastName}!");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int percentGrade = int.Parse(grade);
+
+        string letterGrade;
+        if (percentGrade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percentGrade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentGrade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentGrade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}.");
+        if (percentGrade >= 70)
+        {
+            Console.WriteLine("You have passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You have failed the course.");
+        }
+
+
     }
 }
+
+
+
